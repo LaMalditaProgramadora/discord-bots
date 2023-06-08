43c861d4a3cf982808b1e68ed372baaa3dbec75d
Jimena Alexandra Ruiz Cerna <jruiz@novatronic.com>
2023-06-07 21:37:12 -0500

    fix crono-bot

--- crono-bot/index.js
+++ crono-bot/index.js
@@ -15,8 +15,12 @@ const client = new Client({
 client.on(Events.ClientReady, () => {
   console.log("crono-bot está en línea");
   // Pattern:
-  // "second(0-59) minute(0-59) hour(0-23) dayOfMonth(1-31) month(1-12) dayOfWeek(0-6)
-  startReminderJob(client, "0 0 13 * * 0-6");
+  // second(0-59) minute(0-59) hour(0-23) dayOfMonth(1-31) month(1-12) dayOfWeek(0-6)
+  //    * => Para cada intervalo
+  //    / => Para cada x tiempo
+  //    - => Para especificar un rango de valores
+  // startReminderJob(client, "0 0 13 * * 0-6"); // En una fecha exacta
+  startReminderJob(client, "0 */1 * * * 0-6"); // Cada minuto
 });
 
 client.login(process.env.DISCORD_TOKEN);
--- crono-bot/src/services/crono.service.js
+++ crono-bot/src/services/crono.service.js
@@ -7,7 +7,7 @@ export const startReminderJob = (client, pattern) => {
   let reminderJob = new CronJob(pattern, () => {
     client.channels.cache
       .get(process.env.DISCORD_CHANNEL_ID)
-      .send("Esto es un recordatorio :D");
+      .send(new Date() + "Esto es un recordatorio :D");
   });
   reminderJob.start();
 };
